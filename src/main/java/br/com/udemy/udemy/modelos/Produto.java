e0415f6cd790b898cd1f14f361f168082d5f18bf
romulo nishiyama <romulonishiyama@gmail.com>
2023-06-14 20:41:52 -0300

    2

---
+++ src/main/java/br/com/udemy/udemy/modelos/Produto.java
@@ -0,0 +1,4 @@
+package br.com.udemy.udemy.modelos;
+
+public class Produto {
+}
--- src/main/java/br/com/udemy/udemy/modelos/Usuario.java
+++ src/main/java/br/com/udemy/udemy/modelos/Usuario.java
@@ -1,12 +1,16 @@
 package br.com.udemy.udemy.modelos;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "usuarios")
+@Getter
+@Setter
 public class Usuario {
 
     public Usuario(){}
@@ -16,74 +20,17 @@ public class Usuario {
     private int id;
 
     private String nome;
-
     private String email;
-
     private String telefone;
-
     private String senha;
-
     private Date data = new Date();
-
-
-
-    public Date getData() {
-        return data;
-    }
-
-    public void setData(Date data) {
-        this.data = data;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 
     public Usuario(String nome, String email, String telefone, String senha) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
         this.senha = senha;
-
-
     }
-
     @Override
     public String toString() {
         return "Usuario{" +
