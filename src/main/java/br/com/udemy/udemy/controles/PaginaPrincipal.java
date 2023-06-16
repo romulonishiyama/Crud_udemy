862fa7520766d3aba84cb7c5705250a2db07d4f8
romulo nishiyama <romulonishiyama@gmail.com>
2023-06-15 21:20:29 -0300

    4

---
+++ src/main/java/br/com/udemy/udemy/controles/PaginaPrincipal.java
@@ -0,0 +1,37 @@
+package br.com.udemy.udemy.controles;
+
+import br.com.udemy.udemy.modelos.Usuario;
+import br.com.udemy.udemy.repositorios.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/cadastro")
+public class PaginaPrincipal {
+
+    @Autowired
+    UsuarioRepositorio usuarioRepositorio;
+
+    @GetMapping
+    public String paginaCadastrar(){
+        return "cadastrar";
+    }
+
+    @GetMapping("/lista")
+    public String lisarUsuarios(Model model){
+        model.addAttribute("lista", usuarioRepositorio.findAll());
+
+        return "listagem";
+
+    }
+
+    @PostMapping
+    public String cadastrarUsuario( Usuario u){
+        usuarioRepositorio.save(u);
+        return "cadastrar";
+    }
+
+
+}
